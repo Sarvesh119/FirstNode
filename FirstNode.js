e0c921af421ca6489ef944bd96dd0659828d4ed6
Sarvesh119 <sarveshkmishra119@gmail.com>
2025-05-04 13:50:21 +0530

    first commit

---
+++ FirstNode.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const fileContent = fs.readFileSync('class17.html','utf-8');
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.end(fileContent);
+})
+server.listen(80,'127.0.0.1',()=>{
+    console.log("Hello Sarvesh Mishra ,wellcome to your first Node Class");
+})
